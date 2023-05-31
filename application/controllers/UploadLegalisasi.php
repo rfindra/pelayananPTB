65c7d6f528cc434aa416f546bbae06681a459f03
Cyber Panel <info@pt-bandung.go.id>
2023-05-31 14:37:18 +0700

    new upload array

--- application/controllers/UploadLegalisasi.php
+++ application/controllers/UploadLegalisasi.php
@@ -25,7 +25,7 @@ class UploadLegalisasi extends CI_Controller {
   
     function uploadImages($id, $view = 'web') {
         $config = [
-            'upload_path' => 'uploads/',
+            'upload_path' => './uploads/',
             'allowed_types' => 'gif|jpg|jpeg|png|pdf',
             'max_size' => '10000',
             'max_width' => '20000',
@@ -33,34 +33,58 @@ class UploadLegalisasi extends CI_Controller {
             'remove_spaces' => false,
         ];
     
+        $this->load->library('upload', $config);
         $this->upload->initialize($config);
-        
+    
         $data = [];
     
-        if (isset($_FILES['gambar'])) {
-            $files = $_FILES['gambar'];
-            $count = count($files['name']);
-            
-            for ($i = 0; $i < $count; $i++) {
-                $_FILES['file']['name'] = $files['name'][$i];
-                $_FILES['file']['type'] = $files['type'][$i];
-                $_FILES['file']['tmp_name'] = $files['tmp_name'][$i];
-                $_FILES['file']['error'] = $files['error'][$i];
-                $_FILES['file']['size'] = $files['size'][$i];
-                
-                if (!$this->upload->do_upload('file')) {
-                    echo $this->upload->display_errors();
-                    return;
-                } else {
-                    $data[$i]['file'] = 'uploads/' . $this->upload->data('file_name');
+        $gambarInputs = ['gambar_ktp', 'gambar_kta', 'gambar_bas', 'gambar_surat_kuasa'];
+    
+        foreach ($gambarInputs as $inputName) {
+            if (isset($_FILES[$inputName])) {
+                $files = $_FILES[$inputName];
+                $count = count($files['name']);
+    
+                for ($i = 0; $i < $count; $i++) {
+                    $_FILES['file']['name'] = $files['name'][$i];
+                    $_FILES['file']['type'] = $files['type'][$i];
+                    $_FILES['file']['tmp_name'] = $files['tmp_name'][$i];
+                    $_FILES['file']['error'] = $files['error'][$i];
+                    $_FILES['file']['size'] = $files['size'][$i];
+    
+                    $fileType = $_FILES['file']['type'];
+                    $uploadPath = "./uploads/{$fileType}/";
+    
+                    if (!file_exists($uploadPath)) {
+                        mkdir($uploadPath, 0777, true);
+                    }
+    
+                    $this->upload->initialize([
+                        'upload_path' => $uploadPath,
+                        'allowed_types' => $config['allowed_types'],
+                        'max_size' => $config['max_size'],
+                        'max_width' => $config['max_width'],
+                        'max_height' => $config['max_height'],
+                        'remove_spaces' => $config['remove_spaces'],
+                    ]);
+    
+                    if (!$this->upload->do_upload('file')) {
+                        $error = $this->upload->display_errors();
+                        // Handle upload error here, e.g. redirect with error message
+                        redirect("Legalisasibas/index{$viewPath}?error={$error}");
+                        return;
+                    } else {
+                        $data[$i]['file'] = "{$fileType}/" . $this->upload->data('file_name');
+                    }
                 }
             }
         }
     
         $this->berkas_model->updateData('berkas', $data, $id);
-        
+    
         $viewPath = $view === 'admin' ? 'admin/' : 'web/';
         redirect("Legalisasibas/index{$viewPath}");
     }
+
     
 }
