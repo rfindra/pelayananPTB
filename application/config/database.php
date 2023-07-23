8c86e9886331e5c3f7b518b12f651a74fb1a253b
indra <if.indra.rf@gmail.com>
2023-07-23 13:52:33 -0700

    add db connection group
    ptsp db

--- application/config/database.php
+++ application/config/database.php
@@ -94,4 +94,26 @@ $db['default'] = array(
 	'failover' => array(),
 	'save_queries' => TRUE
 );
+
+$db['ptsp'] = array(
+	'dsn'	=> '',
+	'hostname' => 'localhost',
+	'username' => 'ptsp_admin',
+	'password' => 'korbe1019',
+	'database' => 'ptsp_db',
+	'dbdriver' => 'mysqli',
+	'dbprefix' => '',
+	'pconnect' => FALSE,
+	'db_debug' => (ENVIRONMENT !== 'production'),
+	'cache_on' => FALSE,
+	'cachedir' => '',
+	'char_set' => 'utf8mb4',
+	'dbcollat' => 'utf8mb4_unicode_ci',
+	'swap_pre' => '',
+	'encrypt' => FALSE,
+	'compress' => FALSE,
+	'stricton' => FALSE,
+	'failover' => array(),
+	'save_queries' => TRUE
+);
 /*Test Commit*/
